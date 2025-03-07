fe2d939d8ed506283c5e2794fbddbdaf656925f7
NielsD1 <n.dijkstra32@icloud.com>
2025-03-07 14:54:24 +0000

    chore: removed old.roverlib

--- old.roverlib/src/callbacks.py
+++
@@ -1,7 +0,0 @@
-import signal
-from typing import Callable
-from configuration import Service, ServiceConfiguration
-
-MainCallback = Callable[[Service, ServiceConfiguration], Exception]
-
-TerminationCallback = Callable[[signal.Signals], Exception]
